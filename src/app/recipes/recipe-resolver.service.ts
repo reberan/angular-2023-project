f1b15cf4741fb143af3b5f3e283714792b2e3104
reberan <reberan@gmail.com>
2023-07-12 13:51:50 +0200

    refactor: :recycle: replaced the deprecated Resolve<T> interface with the ResolveFn function of @angular/router

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  Resolve,
+  //Resolve, /* deprecated */
+  ResolveFn,
   RouterStateSnapshot,
 } from '@angular/router';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { map, take, switchMap } from 'rxjs/operators';
 
 import AppState from '../store/app.state';
@@ -14,63 +15,45 @@ import RecipesState from './store/recipes.state';
 import { Recipe } from './recipe.model';
 import * as RecipesActions from './store/recipes.actions';
 
-@Injectable({ providedIn: 'root' })
-export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(private actions$: Actions, private store: Store<AppState>) {}
+export const recipesResolver: ResolveFn<Recipe[]> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+  actions$: Actions = inject(Actions),
+  store: Store<AppState> = inject(Store<AppState>)
+): any =>
+  store.select('recipes').pipe(
+    take(1),
+    map((recipesState: RecipesState) => recipesState.recipes),
+    switchMap((recipes: Recipe[]) => {
+      if (recipes.length > 0) {
+        return of(recipes);
+      } else {
+        store.dispatch(new RecipesActions.FetchRecipes());
+        return actions$.pipe(ofType(RecipesActions.SET_RECIPES), take(1));
+      }
+    })
+  );
 
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.store.select('recipes').pipe(
-      take(1),
-      map((recipesState: RecipesState) => recipesState.recipes),
-      switchMap((recipes: Recipe[]) => {
-        if (recipes.length > 0) {
-          return of(recipes);
-        } else {
-          this.store.dispatch(new RecipesActions.FetchRecipes());
-          return this.actions$.pipe(
-            ofType(RecipesActions.SET_RECIPES),
-            take(1)
-          );
-        }
-      })
-      /*switchMap((recipes: Recipe[]) => {
-        if (recipes.length === 0) {
-          return this.actions$.pipe(
-            ofType(RecipesActions.SET_RECIPES),
-            take(1)
-          );
-        } else {
-          return of(recipes);
-        }
-      }*/
-    );
-  }
+// /* deprecated */
+// @Injectable({ providedIn: 'root' })
+// export class RecipeResolverService implements Resolve<Recipe[]> {
+//   constructor(private actions$: Actions, private store: Store<AppState>) {}
 
-  // resolve(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot
-  // ):
-  //   | Observable<{ recipes: Recipe[] }>
-  //   | Promise<{ recipes: Recipe[] }>
-  //   | { recipes: Recipe[] } {
-  //   this.store.select('recipes').pipe(
-  //     take(1),
-  //     map((recipesState: RecipesState) => recipesState.recipes),
-  //     switchMap((recipes: Recipe[]) => {
-  //       if (recipes.length === 0) {
-  //         this.store.dispatch(new RecipesActions.FetchRecipes());
-  //         return this.actions$.pipe(
-  //           ofType(RecipesActions.SET_RECIPES),
-  //           take(1)
-  //         );
-  //       } else {
-  //         return of({ recipes });
-  //       }
-  //     })
-  //   );
-  //   return this.store.dispatch(new RecipesActions.FetchRecipes());
-  // }
-}
+//   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
+//     return this.store.select('recipes').pipe(
+//       take(1),
+//       map((recipesState: RecipesState) => recipesState.recipes),
+//       switchMap((recipes: Recipe[]) => {
+//         if (recipes.length > 0) {
+//           return of(recipes);
+//         } else {
+//           this.store.dispatch(new RecipesActions.FetchRecipes());
+//           return this.actions$.pipe(
+//             ofType(RecipesActions.SET_RECIPES),
+//             take(1)
+//           );
+//         }
+//       })
+//     );
+//   }
+// }
